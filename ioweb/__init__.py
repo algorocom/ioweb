1b97d748d4e419e2a6b43f4e176d299624d43e68
Gregory Petukhov <lorien@lorien.name>
2019-07-15 21:42:13 +0300

    Bump version: 0.0.5 → 0.0.6

--- ioweb/__init__.py
+++ ioweb/__init__.py
@@ -4,7 +4,7 @@ Monkey patching is done by `ioweb` console command
 which is imported from `ioweb_gevent` package
 in setup.py entry point
 """
-__version__ = '0.0.5'
+__version__ = '0.0.6'
 
 from .session import Session
 from .request import Request, CallbackRequest
